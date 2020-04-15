fbc4d615fb8e2991b3f3c54d53b357133bfa2f5e
ricardo3853 <rssantan@gmail.com>
2020-04-15 10:50:14 -0300

    Primeiro Commit

---
+++ templates/password_lost_form.php
@@ -0,0 +1,34 @@
+<div id="password-lost-form" class="widecolumn">
+	<?php if ( $attributes['show_title'] ) : ?>
+		<h3><?php _e( 'Forgot Your Password?', 'personalize-login' ); ?></h3>
+	<?php endif; ?>
+
+	<?php if ( count( $attributes['errors'] ) > 0 ) : ?>
+		<?php foreach ( $attributes['errors'] as $error ) : ?>
+			<p>
+				<?php echo $error; ?>
+			</p>
+		<?php endforeach; ?>
+	<?php endif; ?>
+
+	<p>
+		<?php
+			_e(
+				"Digite seu endereço de e-mail e enviaremos um link para você escolher uma nova senha.",
+				'personalize_login'
+			);
+		?>
+	</p>
+
+	<form id="lostpasswordform" action="<?php echo wp_lostpassword_url(); ?>" method="post">
+		<p class="form-row">
+			<label for="user_login"><?php _e( 'Email', 'personalize-login' ); ?>
+			<input type="text" name="user_login" id="user_login">
+		</p>
+
+		<p class="lostpassword-submit">
+			<input type="submit" name="submit" class="lostpassword-button"
+			       value="<?php _e( 'Reset Password', 'personalize-login' ); ?>"/>
+		</p>
+	</form>
+</div>
---
+++ templates/register_form.php
@@ -0,0 +1,60 @@
+<div id="register-form" class="widecolumn">
+	<?php if ( $attributes['show_title'] ) : ?>
+		<h3><?php _e( 'Register', 'personalize-login' ); ?></h3>
+	<?php endif; ?>
+
+	<?php if ( count( $attributes['errors'] ) > 0 ) : ?>
+		<?php foreach ( $attributes['errors'] as $error ) : ?>
+			<p class='login-error'>
+				<?php echo $error; ?>
+			</p>
+		<?php endforeach; ?>
+	<?php endif; ?>
+
+	<form id="signupform" action="<?php echo wp_registration_url(); ?>" method="post">
+		<fieldset>
+		 	<legend>Registre-se</legend>					
+			 
+					<div class="row">
+						<div class="col-md-5">
+							<input type="radio" name="tipo_role" id='role-jovem-aprendiz' value="jovem_aprendiz" checked>
+							<label for='role-jovem-aprendiz'>SOU UM JOVEM APRENDIZ</label>
+						</div>
+						<div class="col-md-5">
+							<input type="radio" name="tipo_role" id='role-empresas' value="empresas">
+							<label for='role-empresas'>SOU UMA EMPRESA</label>
+						</div>
+					</div>
+				<p></p>
+			<p class="form-row">
+				<label for="email"><?php _e( 'Email', 'personalize-login' ); ?> <strong>*</strong></label>
+				<input type="text" name="email" id="email">
+			</p>
+			<p class="form-row">
+				<label for="first_name"><?php _e( 'First name', 'personalize-login' ); ?></label>
+				<input type="text" name="first_name" id="first-name">
+			</p>
+
+			<p class="form-row">
+				<label for="last_name"><?php _e( 'Last name', 'personalize-login' ); ?></label>
+				<input type="text" name="last_name" id="last-name">
+			</p>
+		
+			<p class="form-row">
+				<?php _e( 'Note: Your password will be generated automatically and emailed to the address you specify above.', 'personalize-login' ); ?>
+			</p>
+
+			<?php if ( $attributes['recaptcha_site_key'] ) : ?>
+				<div class="recaptcha-container">
+					<div class="g-recaptcha" data-sitekey="<?php echo $attributes['recaptcha_site_key']; ?>"></div>
+				</div>
+			<?php endif; ?>
+		<p></p>	 
+		<p class="signup-submit">
+			<input type="submit" name="submit" class="register-button"
+			       value="<?php _e( 'Register', 'personalize-login' ); ?>"/>
+		</p>
+	</fieldset>
+
+	</form>
+</div>
